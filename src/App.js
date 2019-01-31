72b991f1e98efbd1017eec7c90f116304fe5cb45
annazus <suzanna@appliedar.us>
2019-01-31 12:23:54 -0800

    set up env variables for api keys

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>Display alternative fuel statios ner me.</p>
+          <p>Display alternative fuel stations near me.</p>
         </header>
       </div>
     );
