36ab15a30858e0f9585213d80c797b6f4493ec67
annazus <suzanna@appliedar.us>
2019-02-01 14:35:24 -0800

    added fuel station reducer

---
+++ src/reducers/altFuelReducers.js
@@ -0,0 +1,17 @@
+import * as types from "../constants/types";
+
+const INITIAL_STATE = {
+  isFetching: false,
+  fuelStations: []
+};
+
+export default function(state = INITIAL_STATE, action) {
+  switch (action.type) {
+    case types.REQUEST_NEARBY_STATIONS:
+      return { ...state, isFetching: true };
+    case types.RECEIVE_NEARBY_STATIONS:
+      return { ...state, isFetching: false, fuelStations: action.payload };
+    default:
+      return state;
+  }
+}
